68e34efb02664a0700cc9fc219cf363f10c7c3ed
unkn0wn-root <unkn0wn_root@tuta.io>
2024-11-14 15:04:53 +0100

    chore: remove unused logging

--- pkg/algorithm/round_robin.go
+++ pkg/algorithm/round_robin.go
@@ -2,7 +2,6 @@ package algorithm
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 )
 
@@ -16,16 +15,6 @@ func (rr *RoundRobin) NextServer(pool ServerPool, _ *http.Request) *Server {
 	servers := pool.GetBackends()
 	if len(servers) == 0 {
 		return nil
-	}
-
-	for _, server := range servers {
-		log.Printf("Server %s: Weight=%d, CurrentWeight=%d, Alive=%v, Connections=%d/%d",
-			server.URL,
-			server.Weight,
-			server.CurrentWeight.Load(),
-			server.Alive.Load(),
-			server.ConnectionCount,
-			server.MaxConnections)
 	}
 
 	currentIdx := pool.GetCurrentIndex()
