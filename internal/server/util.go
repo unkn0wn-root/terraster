06249af81db73d564e1524ff3048fd684718bdd0
unkn0wn-root <unkn0wn_root@tuta.io>
2024-11-19 14:38:41 +0100

    chore: check if host includes ':' for fast path

--- internal/server/util.go
+++ internal/server/util.go
@@ -4,36 +4,28 @@ import (
 	"crypto/tls"
 	"net"
 	"strconv"
+	"strings"
 )
 
 func parseHostPort(hostPort string, tlsState *tls.ConnectionState) (host string, port int, err error) {
+	// Fast path for common case: no port specified
+	if !strings.Contains(hostPort, ":") {
+		if tlsState != nil {
+			return hostPort, DefaultHTTPSPort, nil
+		}
+		return hostPort, DefaultHTTPPort, nil
+	}
+
+	// Slow path: parse host:port
 	host, portStr, err := net.SplitHostPort(hostPort)
 	if err != nil {
-		host = hostPort
+		return "", 0, err
 	}
 
-	// portStr will be empty if there is no port in the hostPort string
-	// we have to assume that request is either http or https on standard port
-	var pn int
-	if portStr == "" {
-		pn = 0
-	} else {
-		p, err := strconv.Atoi(portStr)
-		if err != nil {
-			return "", 0, err
-		}
-
-		pn = p
+	port, err = strconv.Atoi(portStr)
+	if err != nil {
+		return "", 0, err
 	}
 
-	// if port is 0, then assign standard http or https port
-	if pn == 0 {
-		if tlsState != nil {
-			pn = DefaultHTTPSPort
-		} else {
-			pn = DefaultHTTPPort
-		}
-	}
-
-	return host, pn, nil
+	return host, port, nil
 }
